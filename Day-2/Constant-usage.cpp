6404b60dcb7ae2e74a8c45e533cf6d8c22534990
muskaan1212 <aria18jane@gmail.com>
2025-07-19 15:26:07 +0530

    Create Constant-usage.cpp

---
+++ Day-2/Constant-usage.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#define pi 3.141
+
+int main() {
+    const double PI_CONST = 3.14159;  
+
+    cout << "Using #define: " << pi<< endl;
+    cout << "Using const: " << PI_CONST << endl;
+
+    return 0;
+}
